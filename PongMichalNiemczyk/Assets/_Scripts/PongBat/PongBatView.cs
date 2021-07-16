5e5b5e337fd33089ba50a4cd6a19d442808bad27
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-07-16 15:15:38 +0200

    Refactor the PongBatView script.

--- PongMichalNiemczyk/Assets/_Scripts/PongBat/PongBatView.cs
+++ PongMichalNiemczyk/Assets/_Scripts/PongBat/PongBatView.cs
@@ -6,12 +6,6 @@ namespace _Scripts.PongBat
     {
         [SerializeField] private Rigidbody2D _rigidbody2D;
 
-        public Vector3 Position
-        {
-            get => transform.position;
-            set => transform.position = value;
-        }
-
         public Rigidbody2D Rigidbody2D
         {
             get => _rigidbody2D;
