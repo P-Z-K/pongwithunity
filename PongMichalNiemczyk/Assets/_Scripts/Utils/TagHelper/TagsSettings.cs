4e1a6897fe9c5ba55913bfffee7e3894e6b2e80d
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-07-20 13:00:59 +0200

    Add tag for players' pong bats and set it in the PongBat variants in the hierarchy.

--- PongMichalNiemczyk/Assets/_Scripts/Utils/TagHelper/TagsSettings.cs
+++ PongMichalNiemczyk/Assets/_Scripts/Utils/TagHelper/TagsSettings.cs
@@ -6,7 +6,7 @@ namespace _Scripts.Utils
     [Serializable]
     public class TagsSettings
     {
-        [TagSelector] public string PlayerTag;
+        [TagSelector] public string PlayerPongBatTag;
         [TagSelector] public string WallTag;
         [TagSelector] public string PlayerHoleTag;
     }
