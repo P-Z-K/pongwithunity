43a67d37c27ca7e43c60794da5072c1e772eb0c1
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-07-25 13:21:44 +0200

    Implement ParticleEntity script.

--- PongMichalNiemczyk/Assets/_Scripts/Particles/ParticleEntity.cs
+++ PongMichalNiemczyk/Assets/_Scripts/Particles/ParticleEntity.cs
@@ -1,10 +1,34 @@
 using UnityEngine;
-using Zenject;
 
 namespace _Scripts.Particles
 {
     public class ParticleEntity : MonoBehaviour
     {
-        [SerializeField] private ParticleSystem _particleSystem;
+        [SerializeField] protected ParticleSystem _particleSystem;
+        public Vector3 Position
+        {
+            get => transform.position;
+            set => transform.position = value;
+        }
+
+        public void Play()
+        {
+            Active = true;
+            _particleSystem.Play();
+        }
+
+        public bool Active
+        {
+            get => gameObject.activeSelf;
+            set => gameObject.SetActive(value);
+        }
+
+        private void Update()
+        {
+            if (!_particleSystem.isPlaying)
+            {
+                // Return object to the pool
+            }
+        }
     }
 }
