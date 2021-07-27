e0f7fa923aff1a6c6da11c8e7970123bf70efa2d
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-07-27 11:26:12 +0200

    Refactor functions associated with signals in BallStateMoving in order to properly unsubscribing signals.

--- PongMichalNiemczyk/Assets/_Scripts/Ball/BallStates/BallStateMoving.cs
+++ PongMichalNiemczyk/Assets/_Scripts/Ball/BallStates/BallStateMoving.cs
@@ -20,19 +20,53 @@ namespace _Scripts.Ball
             _signalBus = signalBus;
             _tagsSettings = tagsSettings;
             _ballView = ballView;
-
-            SubscribeSignals();
         }
 
         private void SubscribeSignals()
         {
-            _signalBus.Subscribe<BallTriggerEntered2DSignal>(x => OnTriggerEnter2D(x.Other));
-            _signalBus.Subscribe<BallCollisionEntered2DSignal>(x => OnCollisionEnter2D(x.Other));
+            _signalBus.Subscribe<BallTriggerEntered2DSignal>(OnTriggerEnter2D);
+            _signalBus.Subscribe<BallCollisionEntered2DSignal>(OnCollisionEnter2D);
+        }
+
+        private void UnsubscribeSignals()
+        {
+            _signalBus.Unsubscribe<BallTriggerEntered2DSignal>(OnTriggerEnter2D);
+            _signalBus.Unsubscribe<BallCollisionEntered2DSignal>(OnCollisionEnter2D);
+        }
+        
+        private void OnCollisionEnter2D(BallCollisionEntered2DSignal obj)
+        {
+            Collision2D other = obj.Other;
+            
+            if (other.gameObject.CompareTag(_tagsSettings.WallTag))
+            {
+                Debug.Log("<color=lime>[BALL INFO]</color> The Ball hit the wall");
+                
+                _signalBus.Fire(new BallHitWallSignal(_ballView.Position));
+                _ballMovement.AddRandomFactorToDirection();
+            }
+            else if (other.gameObject.CompareTag(_tagsSettings.PlayerPongBatTag))
+            {
+                Debug.Log("<color=lime>[BALL INFO]</color> The Ball hit the pong bat");
+                _signalBus.Fire(new BallHitPongBatSignal(_ballView.Position));
+            }
+        }
+
+        private void OnTriggerEnter2D(BallTriggerEntered2DSignal obj)
+        {
+            Collider2D other = obj.Other;
+            
+            if (other.gameObject.CompareTag(_tagsSettings.PlayerHoleTag))
+            {
+                Debug.Log("<color=lime>[BALL INFO]</color> The ball fell into the player hole");
+                _owner.ChangeStateTo<BallStateInPlayerHole>();
+            }
         }
 
         public override void EnterState()
         {
             Debug.Log("<color=lime>[BALL STATE]</color> Ball starts moving...");
+            SubscribeSignals();
             _ballMovement.StartMove();
         }
 
@@ -47,31 +81,7 @@ namespace _Scripts.Ball
 
         public override void ExitState()
         {
-        }
-
-        private void OnTriggerEnter2D(Collider2D other)
-        {
-            if (other.gameObject.CompareTag(_tagsSettings.PlayerHoleTag))
-            {
-                Debug.Log("<color=lime>[BALL INFO]</color> The ball fell into the player hole");
-                _owner.ChangeStateTo<BallStateInPlayerHole>();
-            }
-        }
-
-        private void OnCollisionEnter2D(Collision2D other)
-        {
-            if (other.gameObject.CompareTag(_tagsSettings.WallTag))
-            {
-                Debug.Log("<color=lime>[BALL INFO]</color> The Ball hit the wall");
-                
-                _signalBus.Fire(new BallHitWallSignal(_ballView.Position));
-                _ballMovement.AddRandomFactorToDirection();
-            }
-            else if (other.gameObject.CompareTag(_tagsSettings.PlayerPongBatTag))
-            {
-                Debug.Log("<color=lime>[BALL INFO]</color> The Ball hit the pong bat");
-                _signalBus.Fire(new BallHitPongBatSignal(_ballView.Position));
-            }
+            UnsubscribeSignals();
         }
     }
 }
