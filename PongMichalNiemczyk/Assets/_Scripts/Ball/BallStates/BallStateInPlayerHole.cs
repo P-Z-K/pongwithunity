1a15942f17426562818fd7a52ae5a776dfcd603a
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-07-27 11:18:52 +0200

    Add triggering signal in BallStateInPlayerHole script. Moreover, in the particle manager add subscribing and unsubscribing this signal.

--- PongMichalNiemczyk/Assets/_Scripts/Ball/BallStates/BallStateInPlayerHole.cs
+++ PongMichalNiemczyk/Assets/_Scripts/Ball/BallStates/BallStateInPlayerHole.cs
@@ -1,22 +1,30 @@
 using _Scripts.Root;
 using UnityEngine;
+using Zenject;
 
 namespace _Scripts.Ball
 {
     public class BallStateInPlayerHole : State<BallStateManager>
     {
         private readonly BallMovement _ballMovement;
+        private readonly BallView _ballView;
+        private readonly SignalBus _signalBus;
 
-        public BallStateInPlayerHole(BallStateManager owner, BallMovement ballMovement)
+        public BallStateInPlayerHole(BallStateManager owner, BallMovement ballMovement, BallView ballView, 
+            SignalBus signalBus)
             : base(owner)
         {
             _ballMovement = ballMovement;
+            _ballView = ballView;
+            _signalBus = signalBus;
         }
 
         public override void EnterState()
         {
             Debug.Log("<color=lime>[BALL STATE]</color> Ball is in player hole...");
+            
             _ballMovement.StopMove();
+            _signalBus.Fire(new BallFellIntoPlayerHoleSignal(_ballView.Position));
         }
 
         public override void Tick()
--- PongMichalNiemczyk/Assets/_Scripts/Particles/ParticleEntityManager.cs
+++ PongMichalNiemczyk/Assets/_Scripts/Particles/ParticleEntityManager.cs
@@ -9,10 +9,10 @@ namespace _Scripts.Particles
 {
     public class ParticleEntityManager
     {
-        private SignalBus _signalBus;
-        private WallHitParticleEntityPool _wallHitParticleEntityPool;
-        private PongBatHitParticleEntityPool _pongBatHitParticleEntityPool;
-        private BallFallIntoPlayerHoleParticleEntityPool _ballFallIntoPlayerHoleParticleEntityPool;
+        private readonly SignalBus _signalBus;
+        private readonly WallHitParticleEntityPool _wallHitParticleEntityPool;
+        private readonly PongBatHitParticleEntityPool _pongBatHitParticleEntityPool;
+        private readonly BallFallIntoPlayerHoleParticleEntityPool _ballFallIntoPlayerHoleParticleEntityPool;
 
         public ParticleEntityManager(SignalBus signalBus,
             WallHitParticleEntityPool wallHitParticleEntityPool,
@@ -29,12 +29,19 @@ namespace _Scripts.Particles
         {
             _signalBus.Subscribe<BallHitWallSignal>(TEST_PlayWallHitParticle);
             _signalBus.Subscribe<BallHitPongBatSignal>(TEST_PlayPongBatHitParticle);
+            _signalBus.Subscribe<BallFellIntoPlayerHoleSignal>(TEST_PlayBallFallIntoPlayerHoleParticle);
         }
 
         public void UnsubscribeSignals()
         {
             _signalBus.Unsubscribe<BallHitWallSignal>(TEST_PlayWallHitParticle);
             _signalBus.Unsubscribe<BallHitPongBatSignal>(TEST_PlayPongBatHitParticle);
+            _signalBus.Unsubscribe<BallFellIntoPlayerHoleSignal>(TEST_PlayBallFallIntoPlayerHoleParticle);
+        }
+        
+        private void TEST_PlayBallFallIntoPlayerHoleParticle(BallFellIntoPlayerHoleSignal obj)
+        {
+            _ballFallIntoPlayerHoleParticleEntityPool.Spawn(obj.BallPosition);
         }
 
         private void TEST_PlayPongBatHitParticle(BallHitPongBatSignal obj)
