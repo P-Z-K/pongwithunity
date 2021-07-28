9f83ea4f5949b745c6635a3c834fcf782ed4750b
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-07-28 19:25:48 +0200

    Create a zenject installer for our menus views.

---
+++ PongMichalNiemczyk/Assets/_Scripts/UI/Menus/MenusInstaller.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using Zenject;
+
+namespace _Scripts.UI.Menus
+{
+    public class MenusInstaller : MonoInstaller
+    {
+        [SerializeField] private StartMenuView _startMenuView;
+        [SerializeField] private GameplayMenuView _gameplayMenuView;
+        [SerializeField] private GameOverMenuView _gameOverMenuView;
+        public override void InstallBindings()
+        {
+            Container.BindInstance(_startMenuView);
+            Container.BindInstance(_gameplayMenuView);
+            Container.BindInstance(_gameOverMenuView);
+        }
+    }
+}
