34cd3346b788ce5f9b577074be4bc1908852ed4f
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-07-30 11:34:56 +0200

    Change menu views into prefabs and add zenject binding components to them instead of binding them in the script installer.

--- PongMichalNiemczyk/Assets/_Scripts/UI/MenusInstaller.cs
+++ PongMichalNiemczyk/Assets/_Scripts/UI/MenusInstaller.cs
@@ -5,18 +5,10 @@ namespace _Scripts.UI.Menus
 {
     public class MenusInstaller : MonoInstaller
     {
-        [SerializeField] private StartMenuView _startMenuView;
-        [SerializeField] private GameplayMenuView _gameplayMenuView;
-        [SerializeField] private GameOverMenuView _gameOverMenuView;
-
         public override void InstallBindings()
         {
             Container.Bind<MenuManager>().AsSingle();
             Container.Bind<UI_PointsTracker>().AsSingle();
-
-            Container.BindInstance(_startMenuView);
-            Container.BindInstance(_gameplayMenuView);
-            Container.BindInstance(_gameOverMenuView);
         }
     }
 }
