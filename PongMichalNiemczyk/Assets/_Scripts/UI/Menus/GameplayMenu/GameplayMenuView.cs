64c280ca4f617c000ce11b92df63a956e1875874
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-08-05 17:48:06 +0200

    Change all occurrences of the basic UnityText component into TextMeshPro components in views.

--- PongMichalNiemczyk/Assets/_Scripts/UI/Menus/GameplayMenu/GameplayMenuView.cs
+++ PongMichalNiemczyk/Assets/_Scripts/UI/Menus/GameplayMenu/GameplayMenuView.cs
@@ -1,16 +1,25 @@
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace _Scripts.UI
 {
     public class GameplayMenuView : MonoBehaviour
     {
-        [SerializeField] private Text _playerOneScoreText;
-        [SerializeField] private Text _playerTwoScoreText;
+        [SerializeField] private TMP_Text _playerOneScoreText;
+        [SerializeField] private TMP_Text _playerTwoScoreText;
+        [SerializeField] private TMP_Text _countdownTimerText;
 
         public void UpdatePlayerOneScoreText(int points) => _playerOneScoreText.text = points.ToString();
 
         public void UpdatePlayerTwoScoreText(int points) => _playerTwoScoreText.text = points.ToString();
+
+        public void UpdateCountdownTimerText(int seconds) => _countdownTimerText.text = seconds.ToString();
+
+        public void ShowCountdownTimer() => _countdownTimerText.enabled = true;
+
+        public void HideCountdownTimer() => _countdownTimerText.enabled = false;
+
+        public bool IsCountdownTimerEnabled => _countdownTimerText.enabled;
 
         public void Show() => gameObject.SetActive(true);
 
