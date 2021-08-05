47ead9eb95a09f7b925d174bb3610c5f8cd187ec
pZk <58830203+P-Z-K@users.noreply.github.com>
2021-08-05 18:12:37 +0200

    Change abstract CompositeComponent class into a normal class and add functionality that iterates through all children in each function

--- PongMichalNiemczyk/Assets/_Scripts/Root/Base classes/Composite/CompositeComponent.cs
+++ PongMichalNiemczyk/Assets/_Scripts/Root/Base classes/Composite/CompositeComponent.cs
@@ -1,23 +1,46 @@
+using System.Collections.Generic;
+
 namespace _Scripts.Composite
 {
-    public abstract class CompositeComponent : IComponent
+    public class CompositeComponent : IComponent
     {
+        private readonly List<IComponent> _children = new List<IComponent>();
+
+        public void AddChild(IComponent child)
+        {
+            _children.Add(child);
+        }
+
         public virtual void Enter()
         {
+            foreach (IComponent child in _children)
+            {
+                child.Enter();
+            }
         }
 
         public virtual void Tick()
         {
+            foreach (IComponent child in _children)
+            {
+                child.Tick();
+            }
         }
 
         public virtual void FixedTick()
         {
+            foreach (IComponent child in _children)
+            {
+                child.FixedTick();
+            }
         }
 
         public virtual void Exit()
         {
+            foreach (IComponent child in _children)
+            {
+                child.Exit();
+            }
         }
-
-        public abstract void AddChild(IComponent child);
     }
 }
